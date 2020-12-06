1805e386fdfecf5e786174a741a09e344d26749a
IanAvery <ian.bick7@gmail.com>
2020-12-05 20:17:07 -0800

    formatting column width and minor edits

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,6 +1,6 @@
         function audioPlayer(){
             var currentSong = 0;
-            $("#audioPlayer")[0].src = $("#playlist li a")[0];
+            $("#audioPlayer")[0].src = $("#playlist li a")[1];
             $("#audioPlayer")[0].play();
             $("#playlist li a").click(function(e){
                e.preventDefault(); 
