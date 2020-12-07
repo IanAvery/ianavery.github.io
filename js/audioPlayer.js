7442cc38f1be34ed14caecb45a87c34f7b4bccf2
IanAvery <ian.bick7@gmail.com>
2020-12-07 01:04:21 -0800

    remove current song from audioPlayer

--- js/audioPlayer.js
+++ js/audioPlayer.js
@@ -1,7 +1,7 @@
         function audioPlayer(){
             var currentSong = 0;
-            $("#audioPlayer")[0].src = $("#playlist li a")[1];
-            $("#audioPlayer")[1].play();
+            $("#audioPlayer")[0].src = $("#playlist li a")[0];
+            $("#audioPlayer")[0].play();
             $("#playlist li a").click(function(e){
                e.preventDefault(); 
                $("#audioPlayer")[0].src = this;
